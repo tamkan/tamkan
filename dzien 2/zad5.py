42c0a436d4a6061517a5beb6cbd7d914e0d5e6e7
Tami <tamarakanecka.tk@gmail.com>
2018-11-27 20:09:25 +0100

    poprawione zadania domowe z dnia#2

--- dzien 2/zad5.py
+++ dzien 2/zad5.py
@@ -4,7 +4,9 @@ szerokosc = int(input("Podaj szerokość prostokąta: "))
 wysokosc = int(input("Podaj wysokość prostokąta: "))
 
 
-print("+" + ("-" * (szerokosc) - 2) + "+")
-print('|' + (" " * (szerokosc - 2)) + ('|\n')*wysokosc, end='')
+print("+" + ("-" * szerokosc) + "+")
+print(('|' + (" " * szerokosc) + '|\n')*wysokosc, end='')
+print("+" + ("-" * szerokosc) + "+")
+
 
 
--- dzien 2/zad6.py
+++ dzien 2/zad6.py
@@ -1,6 +1,8 @@
 print("Program do przeliczania liczb w systemie binarnym na dziesiątkowy")
 
-liczba_bin = int(input("Podaj liczbę w formacie binarnym i 5 indeksach: "))
-liczba_dzies = (liczba_bin[0] * (2**5)) + (liczba_bin[1] * (2**4)) + (liczba_bin[2] * (2**3)) + (liczba_bin[3] * (2**2)) + (liczba_bin[4] * (2**1)) + (liczba_bin[5] * (2**0))
+liczba_bin = input("Podaj liczbę w formacie binarnym i 5 indeksach: ")
 
-print("Twoja liczba w formacie dziesiętnym to: " + liczba_dzies)
+liczba_dzies = (int(liczba_bin[0]) * (2**5)) + (int(liczba_bin[1]) * (2**4)) + (int(liczba_bin[2]) * (2**3)) + (int(liczba_bin[3]) * (2**2)) + (int(liczba_bin[4]) * (2**1)) + (int(liczba_bin[5]) * (2**0))
+
+print("Twoja liczba w formacie dziesiętnym to: " + str(liczba_dzies))
+
